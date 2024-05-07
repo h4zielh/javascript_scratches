7bb0da6fe18d3b216477fb848788f6b069a36d8f
haziel <167656882+h4zielh@users.noreply.github.com>
2024-05-07 17:57:50 -0300

    aaaa

--- main.js
+++ main.js
@@ -7,12 +7,11 @@ let somevar1;
 // js is also case-sensitive, we can sign a value to a variable in its declaration:
 let SomeVar1 = "i'm a string!";
 
-// the === test if both variables are of the same type and has the same value
+// the === (strict equality) test if both variables are of the same type and has the same value
 document.getElementById("4_===_4").textContent = "4" === 4; // this returns false
 
 document.getElementById("4_==_4").textContent = "4" == 4;   // but this returns true
 
-// 
-
-
+// the !== testfor ===, then negate its value, simple huh?
+3 !== "3" // equals to !(3 === "3")
 
