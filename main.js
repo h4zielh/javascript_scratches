2bdcc6697e926ba48dbe8652ba430ecde4204a75
haziel <167656882+h4zielh@users.noreply.github.com>
2024-05-08 18:07:20 -0300

    aaa

--- main.js
+++ main.js
@@ -270,7 +270,7 @@ just like so, we can use continue to go back to the beggining of the current lab
 
 // we can use the for ... in to iterate over a enumerable object like python or C#:
 const sequence1 = [1, 2, 3, 4, 5];
-for (item in sequence1) {
+for (var item in sequence1) {
     console.log(item);
 }
 
