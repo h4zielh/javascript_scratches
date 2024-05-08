287da5666e75d5cb4439ae271fd05511a83cbc6d
haziel <167656882+h4zielh@users.noreply.github.com>
2024-05-08 18:14:47 -0300

    aaa

--- main.js
+++ main.js
@@ -292,3 +292,9 @@ console.log("for..of:");
 for (var item of sequence1) {
     console.log(item); // this logs 1, 2, 3, 4, 5
 }
+
+/*
+The for...of statement creates a loop Iterating over iterable objects (including Array, Map,
+Set, arguments object and so on), invoking a custom iteration hook with statements to be
+executed for the value of each distinct property.
+*/
