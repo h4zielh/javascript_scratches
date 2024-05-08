1cb3d23ac1d0a8ef0520c7cc397bfb49cf064879
haziel <167656882+h4zielh@users.noreply.github.com>
2024-05-08 18:14:07 -0300

    aaa

--- main.js
+++ main.js
@@ -275,7 +275,7 @@ sequence1.variable2 = 20;
 
 console.log("for..in:");
 for (var item in sequence1) {
-    console.log(item);
+    console.log(item); // this logs 0, 1, 2, 3, 4, "variable1", "variable2"
 }
 
 /*
@@ -289,6 +289,6 @@ array elements, if you modify the Array object (such as adding custom properties
 
 // alternatively, we can use for..of to iterate over value properties of an enumerable object:
 console.log("for..of:");
-for (var item in sequence1) {
-    console.log(item);
+for (var item of sequence1) {
+    console.log(item); // this logs 1, 2, 3, 4, 5
 }
