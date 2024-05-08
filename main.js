827ac35e12d39d6ac9dd961ebd814c6a9af713aa
haziel <167656882+h4zielh@users.noreply.github.com>
2024-05-08 05:13:01 -0300

    aaa

--- main.js
+++ main.js
@@ -114,8 +114,9 @@ const array1 = ["one" , "two", "three"];
 // as an array is a reference type, we can modify it's attributes, such as its elements:
 array1.push("four");
 delete array1[1];           // all fine here
-array[2] = "don't know";    // repare here, like python w can use ' inside "", and " inside '', there's
+array[1] = "don't know";    // repare here, like python w can use ' inside "", and " inside '', there's
                             // nothing wrong here because the interpreter can distinguish it from the string quotes
+console.log(array1); // returns ["one", "don't know", "four"]
 
 
 
