37be9663788eb41af89b37eb5adaa75caef84820
haziel <167656882+h4zielh@users.noreply.github.com>
2024-05-09 19:21:16 -0300

    aaa

--- main.js
+++ main.js
@@ -658,7 +658,7 @@ class Myclass3 extends Myclass1 {
 
     func4() {
         // calls func3 from the parent class
-        super func3();
+        super.func3();
     }
 
     // override func1 from the parent class implementation
